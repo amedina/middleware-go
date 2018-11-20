4ea5252b7b999895f6b213d4c08c2cc2c6444212
Alberto Medina <aa.medina@gmail.com>
2018-11-19 17:10:16 -0800

    Finish example

--- middleware.go
+++ middleware.go
@@ -1,5 +1,11 @@
 package main
 
+// A middleware in itself simply takes a http.HandlerFunc as one of
+// its parameters, wraps it and returns a new http.HandlerFunc for
+// the server to call.
+
+// https://medium.com/@matryer/writing-middleware-in-golang-and-how-go-makes-it-so-much-fun-4375c1246e81
+
 import (
 	"fmt"
 	"log"
@@ -7,10 +13,52 @@ import (
 	"time"
 )
 
+// Middleware type
 type Middleware func(http.HandlerFunc) http.HandlerFunc
 
+// Logging logs all requests with its path and the time it took to process
 func Logging() Middleware {
 	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			// do middleware things
+			start := time.Now()
+			defer func() { log.Println(r.URL.Path, time.Since(start)) }()
 
+			f(w, r)
+		}
 	}
+}
+
+// Method ensures that url can only be requested with a specific method, else returns a 400 Bad Request
+func Method(m string) Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != m {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+			//Call next Middleware/handler in chain
+			f(w, r)
+		}
+	}
+}
+
+// Chain applies middlewares to a http.HandlerFunc
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
+// Hello function
+func Hello(w http.ResponseWriter, r *http.Request) {
+	subject := r.URL.Path[len("/"):]
+	fmt.Fprintf(w, "Hello %s!", subject)
+}
+
+func main() {
+	http.HandleFunc("/", Chain(Hello, Method("GET"), Logging()))
+	http.ListenAndServe(":8080", nil)
 }
